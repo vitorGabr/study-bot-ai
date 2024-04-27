ce91d1bdb1422dfe18a1cef47fe4d58bade36bfd
Vitor Gabriel de Jesus <macstudios.info@gmail.com>
2024-04-27 09:13:48 -0300

    Update channel name in channels.ts

--- src/constants/channels.ts
+++ src/constants/channels.ts
@@ -1,13 +1,34 @@
 type ChannelProps = {
 	id: string;
 	name: string;
+	tag: string;
 	optionalNames?: string[];
 };
 
 export const CHANNELS = [
 	{
-		id: "1233177884995358721",
 		name: "Linguagens Formais e Autômatos",
+		tag: "linguagens-formais-e-automatos",
 		optionalNames: ["Autômatos"],
 	},
+	{
+		name: "Arquitetura de Computadores Modernos",
+		tag: "arquitetura-de-computadores-modernos",
+	},
+	{
+		name: "Sistemas Operacionais",
+		tag: "sistemas-operacionais",
+	},
+	{
+		name: "Inteligência Artificial",
+		tag: "inteligencia-artificial",
+	},
+	{
+		name: "Computação Gráfica",
+		tag: "computacao-grafica",
+	},
+	{
+		name: "Arquitetura de Redes de Computador",
+		tag: "arquitetura-de-redes-de-computador",
+	}
 ] as ChannelProps[];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ChannelType } from "discord.js";
+import { ChannelType, type Channel, type GuildBasedChannel } from "discord.js";
 import dayjs from "dayjs";
 
 import { summarizeImagesContent } from "./services/summarize-images-content";
@@ -8,61 +8,79 @@ import { CHANNELS } from "./constants/channels";
 import { ERRORS } from "./constants/errors";
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client?.user?.tag}!`);
+	console.log(`Logged in as ${client?.user?.tag}!`);
 });
 
 client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
+	if (message.author.bot) return;
 
-    const images = message.attachments.map((attachment) => attachment.url);
-    if (images.length === 0) {
-        console.error(ERRORS.NO_IMAGE_FOUND);
-        return;
-    }
+	const images = message.attachments.map((attachment) => attachment.url);
+	if (images.length === 0) {
+		console.error(ERRORS.NO_IMAGE_FOUND);
+		return;
+	}
 
-    await message.channel.sendTyping();
+	await message.channel.sendTyping();
 
-    try {
-        const content = await summarizeImagesContent(images);
+	try {
+		const content = await summarizeImagesContent(images);
 
-        if (content.images.length === 0) {
-            await message.reply(ERRORS.NO_IMAGE_FOUND);
-            return;
-        }
-        const uniqueSubjects = [...new Set(content.images.map((image) => image.subject))];
-        for (const subject of uniqueSubjects) {
-            const subjects = content.images.filter((image) => image.subject === subject);
-            const generatedClass = await summarizeDay(subjects);
+		if (content.images.length === 0) {
+			await message.reply(ERRORS.NO_IMAGE_FOUND);
+			return;
+		}
+		const uniqueSubjects = [
+			...new Set(content.images.map((image) => image.subject)),
+		];
+		for (const subject of uniqueSubjects) {
+			const subjects = content.images.filter(
+				(image) => image.subject === subject,
+			);
+			const generatedClass = await summarizeDay(subjects);
 
-            const channelInfo = CHANNELS.find((channel) => {
-                return channel.name === subject || (channel.optionalNames?.includes(subject));
-            });
+			const channelInfo = CHANNELS.find((channel) => {
+				return (
+					channel.name === subject || channel.optionalNames?.includes(subject)
+				);
+			});
 
-            if (!channelInfo) {
-                await message.reply(`${ERRORS.CHANNEL_NOT_FOUND}, ${subject} não encontrado!`);
-                continue;
-            }
+			if (!channelInfo) {
+				await message.reply(
+					`${ERRORS.CHANNEL_NOT_FOUND}, ${subject} não encontrado!`,
+				);
+				continue;
+			}
 
-            const channel = client.channels.cache.get(channelInfo.id);
-            if (!channel) {
-                console.error(ERRORS.CHANNEL_NOT_FOUND);
-                continue;
-            }
+			let channel: Channel | undefined = message.guild?.channels.cache.find(
+				(channel) => channel.name === channelInfo.tag,
+			)
 
-            if (channel.type === ChannelType.GuildText) {
-                const date = dayjs().subtract(1, "day").format("DD/MM/YYYY");
-                const topic = await channel.threads.create({ name: `Aula gerada do dia ${date}` });
-                await topic.send({
-                    files: generatedClass.content.map((content) => content.image),
-                    content: generatedClass.text,
-                });
-            }
-        }
+			if (!channel) {
+				channel = client.channels.cache.get(
+                    message.channel.id
+                );
+                if (!channel) {
+                    await message.reply(ERRORS.CHANNEL_NOT_FOUND);
+                    continue;
+                }
+			}
 
-        await message.delete();
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
+			if (channel?.type === ChannelType.GuildText) {
+				const date = dayjs().subtract(1, "day").format("DD/MM/YYYY");
+				const topic = await channel.threads.create({
+					name: `Aula gerada do dia ${date}`,
+				});
+				await topic.send({
+					content: generatedClass.text,
+					files: generatedClass.content.map((content) => content.image),
+				});
+			}
+		}
+
+		await message.delete();
+	} catch (error) {
+		console.error("An error occurred:", error);
+	}
 });
 
 export { client };
