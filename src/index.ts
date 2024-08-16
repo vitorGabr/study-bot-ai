3353b4a9284e517f1d3a0c2ec8ee1825d41111b6
vitorGabr <macstudios.info@gmail.com>
2024-08-16 14:14:13 -0300

    Refactor image attachment handling and summarize content logic

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,5 @@ import { Core } from "./structures/main/core";
 const client = new Core();
 client.start();
 
-Bun.serve({
-	port: 3000,
-	fetch(request) {
-		return new Response("Welcome to Bun!");
-	},
-});
 
 export { client };
