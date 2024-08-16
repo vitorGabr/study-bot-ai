2e81d12c9da870818a3fab6b9f53952adcf5ece4
vitorGabr <macstudios.info@gmail.com>
2024-08-16 13:45:50 -0300

    Refactor dependencies in package.json

--- src/events/main.ts
+++ src/events/main.ts
@@ -1,13 +1,10 @@
 import { ChannelType, EmbedBuilder } from "discord.js";
-import { CHANNELS } from "../settings/constants/channels";
-import { ERRORS } from "../settings/constants/errors";
-import { SumarizeDay } from "../handlers/summarize-day";
-import { SummarizeImagesContent } from "../handlers/summarize-images-content";
+import { CHANNELS } from "../constants/channels";
+import { ERRORS } from "../constants/errors";
+import { ContentSummarizer } from "../handlers/content-summarizer";
 import dayjs from "dayjs";
 import { Event } from "../structures/main/event";
-import { GeminiIa } from "../structures/ai/gemini";
 
-const ia = new GeminiIa();
 
 export default new Event({
 	name: "messageCreate",
@@ -16,16 +13,15 @@ export default new Event({
 
 		if (attachments.size <= 0 || author.bot) return;
 
-		const images = attachments.map((attachment) => attachment.url);
-		const contents = await new SummarizeImagesContent(ia).execute(images);
-		for (const item of contents) {
-			const { subject, content } = item;
-			const contentsText = content.map((c) => c.content)
+		const images = attachments.map((attachment) =>  attachment.url);
+		const contents = await new ContentSummarizer().execute(images);
+
+		for (const item of contents.subjects) {
+			const { subject,resume,imagesIndex } = item;
 			const channelInfo = CHANNELS.find((channel) => channel.name === subject);
 			const channel = guild?.channels.cache.find(
 				(channel) => channel.name === channelInfo?.tag,
 			);
-
 			if (!channel) {
 				await message.reply(
 					`${ERRORS.CHANNEL_NOT_FOUND}, ${subject} não encontrado!`,
@@ -33,23 +29,18 @@ export default new Event({
 				continue;
 			}
 
-			const generatedClass = await new SumarizeDay(ia).execute(contentsText);
-
 			if (channel?.type === ChannelType.GuildText) {
 				const date = dayjs().subtract(1, "day").format("DD/MM/YYYY");
 
 				const embed = new EmbedBuilder()
 					.setTitle(`Aula gerada do dia ${date}`)
-					.setDescription(generatedClass)
+					.setDescription(resume)
 					.setTimestamp()
-					.setColor('Random')
-					.setFooter({
-						text: `Gerado por ${ia.name}`,
-					});
+					.setColor('Random');
 
 				await channel.send({
 					embeds: [embed],
-					files: content.map((c) => c.image),
+					files: imagesIndex.map((index) => images[index]),
 				});
 			}
 		}
---
+++ src/handlers/content-summarizer.ts
@@ -0,0 +1,59 @@
+import { CHANNELS } from "../constants/channels";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
+import { z } from "zod";
+
+export class ContentSummarizer {
+	async execute(images: string[]) {
+		const channels = CHANNELS.map((channel) => channel.name).join(", ");
+		const imagesContent = images.map((image) => ({
+			type: "image",
+			image: new URL(image),
+		})) as { type: "image"; image: URL }[];
+
+		const response = await generateObject({
+			model: google("models/gemini-1.5-pro-latest"),
+			messages: [
+				{
+					role: "user",
+					content: [
+						...imagesContent,
+						{
+							type: "text",
+							text: `Dados: "As matérias disponíveis são: ${channels}."`
+						},
+						{
+							type: "text",
+							text: `
+								Objetivo: "
+                                    1. Dada a sequência de imagens organizar por index, (0,1,2)
+									2. Dada a uma sequência de imagens, agrupar por matéria e gerar um resumo detalhado para cada uma, 
+										o objetivo é identificar o conteúdo passado em sala de aula de cada matéria.
+										O resumo deve ser bem organizado e detalhado, com informações relevantes que ajudem a entender o conteúdo.
+										Agrupar por cada matéria e resumo um array de imagens que representam o conteúdo com o index que foi gerado de cada imagem,
+										por exemplo, a matéria de matemática tem 3 imagens, então o index será (0,1,2) e assim por diante.
+									3. Caso o conteúdo não esteja presente em nenhuma matéria, ele deve ser incluido em Outros explicando detalhadamente sobre o assunto.
+								",
+								Observação: "
+									- O resumo deve ser bem detalhado e organizado, com informações relevantes que ajudem a entender o conteúdo.
+									- Geralmente o conteúdo é passado em sequência de imagems da aula do dia anterior.
+								"
+							`,
+						},
+					],
+				},
+			],
+			schema: z.object({
+				imagesIndex: z.array(z.coerce.number()),
+				subjects: z.array(
+					z.object({
+						subject: z.string(),
+						resume: z.string(),
+						imagesIndex: z.array(z.coerce.number()),
+					}),
+				),
+			}),
+		});
+		return response.object;
+	}
+}
