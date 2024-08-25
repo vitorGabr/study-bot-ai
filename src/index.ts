69ec0fe7a574e4c801fb1c7b2414136814b8a5ea
vitorGabr <macstudios.info@gmail.com>
2024-08-25 12:32:23 -0300

    chore: Update npm dependencies to latest versions

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,4 @@ import { Core } from "./structures/core";
 const client = new Core();
 client.start();
 
-const server = Bun.serve({
-	port: 8080,
-	fetch(req) {
-		return new Response("404!");
-	},
-});
-
-console.log(`${server.url} is running!`);
-
 export { client };
